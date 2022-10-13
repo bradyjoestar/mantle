1044f78b4e27b22a7afd55e59c48cc01da782844
wenbin <wenbin.shi@gmail.com>
2022-10-13 19:33:53 +0800

    fix path

---
+++ ops/goerli/mantle/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r Makefile.goerli.mantle.deploy ../../
+cp -r docker-compose.goerli.mantle.deploy.yml ../../
+
+cd ../../
+make -f Makefile.goerli.mantle.deploy clean
+make -f Makefile.goerli.mantle.deploy up
+
+rm -rf Makefile.goerli.mantle.deploy
+rm -rf docker-compose.goerli.mantle.deploy.yml
