3519c915c4933c2b1a62eb902ab13ecf3e6073f1
wenbin <wenbin.shi@gmail.com>
2022-10-14 13:44:53 +0800

    fix bug

--- ops/goerli/mantle/deploy.sh
+++ ops/goerli/mantle/deploy.sh
@@ -4,6 +4,7 @@ cp -r Makefile.goerli.mantle.deploy ../../
 cp -r docker-compose.goerli.mantle.deploy.yml ../../
 
 cd ../../
+rm -rf .env
 make -f Makefile.goerli.mantle.deploy clean
 make -f Makefile.goerli.mantle.deploy up
 
