016bc9a7e64f0074bff2540fc52e1f1ad2fad105
wenbin <wenbin.shi@gmail.com>
2022-10-14 10:17:58 +0800

    update docker-compose.mantle.sh

---
+++ ops/goerli/mantle/runMantle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r Makefile.goerli.mantle ../../
+cp -r docker-compose.goerli.mantle.yml ../../
+
+cd ../../
+make -f Makefile.goerli.mantle clean
+make -f Makefile.goerli.mantle up
+
+rm -rf Makefile.goerli.mantle
+rm -rf docker-compose.goerli.mantle.yml
